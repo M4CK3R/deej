095e32eeb1d10983d6be413a3d1c3aaffbcb6936
MACKER <macker2001@tlen.pl>
2025-02-18 15:39:24 +0100

    Update .gitignore and modify shell.nix build inputs

--- pkg/deej/shell.nix
+++ pkg/deej/shell.nix
@@ -1,15 +1,10 @@
 { pkgs ? import <nixpkgs> { } }:
-let
-buildInputs=with pkgs; [
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [ pkg-config ];
+  buildInputs = with pkgs; [
     go
-    pkg-config
-    gtk4
+    gtk3
     libayatana-indicator
     libindicator
   ];
-in
-pkgs.mkShell {
-  buildInputs = buildInputs;
-
-  LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath buildInputs}";
 }
